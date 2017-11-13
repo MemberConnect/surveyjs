5c6f977b10301eac0d0a2a429b79d4d1a1ffd0c8
Chris Zamierowski <chriszamierowski@gmail.com>
2017-11-13 07:03:36 -0800

    adding Tanay's matrix changes

--- src/vue/matrix.vue
+++ src/vue/matrix.vue
@@ -1,25 +1,46 @@
 <template>
-    <table :class="question.cssClasses.root">
-        <thead>
-            <tr>
-                <td v-show="question.hasRows"></td>
-                <th v-for="column in question.columns"><survey-string :locString="column.locText"/></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(row, rowIndex) in question.visibleRows" :class="question.cssClasses.row">
-                <td v-show="question.hasRows"><survey-string :locString="row.locText"/></td>
-                <td v-for="(column, columnIndex) in question.columns">
-                    <label :class="question.cssClasses.label">
-                        <input type="radio" :class="question.cssClasses.itemValue" :name="row.fullName" v-model="row.value" :value="column.value" :disabled="question.isReadOnly" :id="(columnIndex === 0) && (rowIndex === 0) ? question.inputId : ''" v-bind:aria-label="question.locTitle.renderedHtml"/>
-                        <span class="circle"></span>
-                        <span class="check"></span>
-                        <span :style="{ 'display': 'none' }">{{question.locTitle.renderedHtml}}</span>
-                    </label>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <table :class="question.cssClasses.root" class="wisely-big-matrix">
+            <thead>
+                <tr>
+                    <th v-show="question.hasRows"></th>
+                    <th v-for="column in question.columns"><survey-string :locString="column.locText"/></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, rowIndex) in question.visibleRows" :class="question.cssClasses.row">
+                    <td v-show="question.hasRows"><survey-string :locString="row.locText"/></td>
+                    <td v-for="(column, columnIndex) in question.columns">
+                        <label :class="question.cssClasses.label" :style="{'margin': '0', 'position': 'absolute'}">
+                            <input type="radio" :class="question.cssClasses.itemValue" :name="row.fullName" v-model="row.value" :value="column.value" :disabled="question.isReadOnly" :id="(columnIndex === 0) && (rowIndex === 0) ? question.inputId : ''"/>
+                            <span class="circle"></span>
+                            <span class="check"></span>
+                        </label>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table :class="question.cssClasses.root" class="wisely-small-matrix">
+            <thead>
+                <tr>
+                    <th v-show="question.hasRows"></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, rowIndex) in question.visibleRows" :class="question.cssClasses.row">
+                    <td v-show="question.hasRows"><survey-string :locString="row.locText"/></td>
+                    <td>
+                        <select :class="question.cssClasses.label" :style="{'margin': '0', 'width': '100%', 'background': '#E9ECF2'}" v-model="row.value">
+                            <option v-for="(column, columnIndex) in question.columns" :class="question.cssClasses.itemValue" :name="row.fullName" :value="column.value" :disabled="question.isReadOnly" :id="(columnIndex === 0) && (rowIndex === 0) ? question.inputId : ''">
+                                <survey-string :locString="column.locText"/>
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script lang="ts">
@@ -33,3 +54,34 @@
     }
     Vue.component("survey-matrix", Matrix)
 </script>
+
+<style>
+
+.wisely-big-matrix {
+    display: table;
+}
+
+.wisely-small-matrix {
+    display: none;
+}
+
+@media screen and (max-width: 767px) {
+    .wisely-big-matrix {
+        display: none;
+    }
+
+    .wisely-small-matrix {
+        display: table;
+    }
+}
+
+@media screen and (max-width: 480px) {
+    .wisely-big-matrix {
+        display: none;
+    }
+
+    .wisely-small-matrix {
+        display: table;
+    }
+}
+</style>
